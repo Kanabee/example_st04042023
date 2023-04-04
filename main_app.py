3214d41df76ef896339a533871ceccb61ccf7d38
Kannarat_beebie <114721110+Kanabee@users.noreply.github.com>
2023-04-04 20:11:29 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,3 +5,8 @@ import pandas as pd
 st.title("Beebie App")
 
 st.write(""" # My First app Hello *world* """)
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('goodbye')
